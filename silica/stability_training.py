a256b2ca5f482a5e9a66b60fb617c92ed40877db
Hazem <hazemessam199@gmail.com>
2024-10-20 07:34:48 +0300

    added the modified script for stability prediction

--- silica/stability_training.py
+++ silica/stability_training.py
@@ -1,3 +1,7 @@
+import os
+os.environ["WANDB_PROJECT"] = "BioxMLHackathon"
+os.environ["WANDB_NOTEBOOK_NAME"] = "/home/ec2-user/SageMaker/stability_pred_dora_norm.ipynb"
+# os.environ["WANDB_MODE"] = "disabled"
 import random
 import numpy as np
 import pandas as pd
@@ -14,6 +18,7 @@ from sklearn import metrics
 from torch.utils.data import Dataset, DataLoader
 from datasets import load_dataset
 from transformers import EvalPrediction, Trainer, TrainingArguments
+import wandb
 
 seed = 7
 random.seed(seed)
@@ -21,34 +26,47 @@ np.random.seed(seed)
 torch.manual_seed(seed)
 
 
-class SingleMutationDataset(Dataset):
-    def __init__(self, df):
-        self.df = df
-
+class SingleMutationDatasetV2(Dataset):
+    def __init__(self, hf_ds, swap=True):
+        self.hf_ds = hf_ds
+        self.swap = swap
+    
     def __len__(self):
-        return self.df.shape[0]
-
+        return len(self.hf_ds)
+    
     def __getitem__(self, idx):
+        wt_seq = self.hf_ds["wt_seq"][idx]
+        mut_seq = self.hf_ds["mut_seq"][idx]
+        pos = self.hf_ds["pos"][idx]
+        label = self.hf_ds["ddg"][idx]
+        
+        if self.swap and random.random() > 0.50:
+            wt_seq, mut_seq = mut_seq, wt_seq
+            label = -label
+
         return {
-            "wt_seq": self.df["wt_seq"][idx],
-            "mut_seq": self.df["mutant_seq"][idx],
-            "position": self.df["position"][idx],
-            "label": self.df["ddG_ML"][idx],
+            "wt_seq": wt_seq,
+            "mut_seq": mut_seq,
+            "position": pos,
+            "label": label,
         }
 
 
 class SingleMutationPooler(nn.Module):
-    def __init__(self):
+    def __init__(self, embed_dim=1536):
         super().__init__()
+        self.wt_weight = nn.Parameter(torch.ones((1, embed_dim)), requires_grad=True)
+        self.mut_weight = nn.Parameter(-1 * torch.ones((1, embed_dim)), requires_grad=True)
+        self.norm = nn.LayerNorm(embed_dim, bias=False)
 
     def forward(self, wt_embedding, mut_embedding, positions):
         embed_shape = wt_embedding.shape[-1]
-        positions = positions.view(-1, 1).unsqueeze(2).repeat(1, 1, embed_shape)
+        positions = positions.view(-1, 1).unsqueeze(2).repeat(1, 1, embed_shape) + 1
         wt_residues = torch.gather(wt_embedding, 1, positions).squeeze(1)
         mut_residues = torch.gather(mut_embedding, 1, positions).squeeze(1)
-        return wt_residues + mut_residues
-
-
+        wt_residues = wt_residues * self.wt_weight
+        mut_residues = mut_residues * self.mut_weight
+        return self.norm(wt_residues + mut_residues)
 
 class StabilityPrediction(nn.Module):
     def __init__(self, backbone, embed_dim=1536):
@@ -60,12 +78,13 @@ class StabilityPrediction(nn.Module):
 
     def reset_parameters(self):
         nn.init.uniform_(self.regressor.weight, -0.01, 0.01)
-
+        nn.init.zeros_(self.regressor.bias)
+    
     def compute_loss(self, logits, labels):
         if labels is None:
             return
         return F.mse_loss(logits, labels)
-
+    
     def forward(self, wt_input_ids, mut_input_ids, positions, labels=None):
         wt_embeddings = self.backbone(sequence_tokens=wt_input_ids).embeddings
         mut_embeddings = self.backbone(sequence_tokens=mut_input_ids).embeddings
@@ -103,17 +122,22 @@ class Collator:
             "positions": positions,
             "labels": labels,
         }
-    
+
+
 def compute_metrics(p: EvalPrediction):
     logits = p.predictions
     labels = p.label_ids
-    spearman_correlation = stats.spearmanr(logits, labels).correlation
-    mae = metrics.mean_absolute_error(logits, labels)
-    mse = metrics.mean_squared_error(logits, labels)
+    spearman = stats.spearmanr(logits, labels).statistic
+    mae = metrics.mean_absolute_error(labels, logits)
+    mse = metrics.mean_squared_error(labels, logits)
+    pearson = stats.pearsonr(logits, labels).statistic[0]
+    rmse = metrics.root_mean_squared_error(labels, logits)
     return {
-        "spearman_correlation": spearman_correlation,
+        "spearman": spearman,
         "mae": mae,
         "mse": mse,
+        "pearson": pearson,
+        "rmse": rmse
     }
 
 
@@ -127,50 +151,78 @@ def main():
     validation = train[train['WT_name'].isin(valid_misfits)]
     validation = validation.reset_index()
 
-    lora_config = LoraConfig(r=16, lora_alpha=32, bias="none", use_dora=False, target_modules=["layernorm_qkv.1"])
+    lora_config = LoraConfig(
+        r=16,
+        lora_alpha=32,
+        bias="none",
+        use_dora=True,
+        target_modules=["layernorm_qkv.1", "ffn.1", "ffn.3"],
+    )
     peft_backbone = get_peft_model(model, lora_config)
 
     downstream_model = StabilityPrediction(peft_backbone).cuda()
 
+
     training_args = TrainingArguments(
-        output_dir="stability_weights",
-        num_train_epochs=10,
+        output_dir="stability_weights_dora_norm_swap_v2",
+        num_train_epochs=20,
         per_device_train_batch_size=1,
         per_device_eval_batch_size=1,
-        warmup_steps=1000,
+        warmup_steps=400,
         learning_rate=1e-04,
         weight_decay=0.0,
-        logging_dir="stability_logs",
+        lr_scheduler_type="cosine",
+        logging_dir="stability_dora_norm_logs_swap_v2",
         logging_steps=10,
         logging_strategy="steps",
         do_train=True,
         do_eval=True,
         eval_strategy="epoch",
         save_strategy="epoch",
-    #     eval_steps=1,
-    #     save_steps=1,
         gradient_accumulation_steps=16,
         fp16=False,
         fp16_opt_level="02",
-        run_name="stability_experiment",
+        run_name="stability_dora_norm_swap_v2_experiment",
         seed=seed,
         load_best_model_at_end=True,
-        metric_for_best_model="spearman_correlation",
+        metric_for_best_model="eval_validation_spearman",
         greater_is_better=True,
         optim="adamw_torch",
         remove_unused_columns=False,
+        report_to="wandb",
+        save_total_limit=1,
     )
 
+    ds = load_dataset(
+        "hazemessam/ddg",
+        data_files={"train": "S2648.csv", "validation": "ssym.csv", "test": "ssym_r.csv"})
+
     collator = Collator(tokenizer)
-    train_ds = SingleMutationDataset(train)
-    eval_ds = SingleMutationDataset(validation)
+    train_ds = SingleMutationDatasetV2(ds["train"], swap=True)
+    eval_ds = SingleMutationDatasetV2(ds["validation"], swap=False)
+    test_ds = SingleMutationDatasetV2(ds["test"], swap=False)
+
+    test_datasets = load_dataset(
+        "hazemessam/ddg",
+        data_files={"train": "myoglobin.csv", "validation": "myoglobin_r.csv", "test": "p53.csv"})
+
+    myoglobin_ds = SingleMutationDatasetV2(test_datasets["train"], swap=False)
+    myoglobin_r_ds = SingleMutationDatasetV2(test_datasets["validation"], swap=False)
+    p53_ds = SingleMutationDatasetV2(test_datasets["test"], swap=False)
+
     trainer = Trainer(
         model=downstream_model,
         args=training_args,
         compute_metrics=compute_metrics,
         data_collator=collator,
         train_dataset=train_ds,
-        eval_dataset=eval_ds,
+        eval_dataset={
+            "validation": eval_ds,
+            "test": test_ds,
+            "myoglobin": myoglobin_ds,
+            "myoglobin_r": myoglobin_r_ds,
+            "p53": p53_ds,
+        },
     )
 
     trainer.train()
